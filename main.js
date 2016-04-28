874f6420cf88846b45fce24e49c7da3c39aa4321
Cade Nichols <cade@cadenichols.com>
2016-04-28 12:15:45 -0700

    you've caught Modal\!

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('pokeApp', [])
-.controller('mainCtrl', function($scope, $http) {
+var app = angular.module('pokeApp', ['ui.bootstrap'])
+.controller('mainCtrl', function($scope, $http, $uibModal) {
 
   $http({
     url: 'http://pokeapi.co/api/v2/pokedex/1/',
@@ -12,7 +12,37 @@ angular.module('pokeApp', [])
   })
   .catch(function(err) {
     console.error(err);
-  })
+  });
 
+  $scope.selectPokemon = function(pokemon) {
+    var modalInstance = $uibModal.open({
+      controller: 'pokeModalCtrl',
+      templateUrl: 'pokeModal.html',
+      resolve: {
+        pokemon: pokemon
+      }
+    });
 
+    modalInstance.result
+    .then(function() {
+      console.log('success!');
+    })
+    .catch(function() {
+      console.log('failure!');
+    })
+  };
 })
+
+.controller('pokeModalCtrl', function($scope, $uibModalInstance, pokemon) {
+  console.log('pokeModalCtrl!', pokemon);
+
+  $scope.pokemon = pokemon;
+
+  $scope.ok = function() {
+    $uibModalInstance.close();
+  };
+
+  $scope.cancel = function() {
+    $uibModalInstance.dismiss();
+  };
+});
